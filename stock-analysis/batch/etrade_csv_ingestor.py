3fa2695dadc4c046e4b529d36748d93211564b20
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2017-06-12 11:03:52 -0700

    Log only version of csv parser batch, minor reorgs on other changes WIP/broken

---
+++ stock-analysis/batch/etrade_csv_ingestor.py
@@ -0,0 +1,69 @@
+import argparse
+from collections import namedtuple
+import csv
+import datetime
+
+
+# TODO handle symbols that are sold
+blacklisted_tickers = ['UAA', 'UA', 'CASY']
+
+# Not sure why AMT is broken in the api
+blacklisted_tickers.append('AMT')
+
+
+Order = namedtuple('Order', ['ticker', 'date', 'num_shares', 'price'])
+
+class RowParserException(Exception):
+    pass
+
+
+class EtradeIngestor(object):
+
+
+    def __init__(self):
+        self.arg_parser = argparse.ArgumentParser(description='Process an etrade csv file')
+        self.arg_parser.add_argument('--csv-path', help='path to csv file')
+
+    def run(self):
+        self.args = self.arg_parser.parse_args()
+        orders = self.parse_orders_from_csv(self.args.csv_path)
+        print(orders)
+
+    def parse_orders_from_csv(self, csv_path):
+        with open(csv_path) as csv_file:
+            reader = csv.reader(csv_file)
+            orders = self._parse_orders_from_csv_reader(reader)
+            return orders
+
+    def _parse_orders_from_csv_reader(self, reader):
+        orders = []
+        for row in reader:
+            try:
+                # Ingore lines that don't parse
+                order = self._extract_order_from_row(row)
+            except RowParserException:
+                continue
+
+            if order:
+                orders.append(order)
+
+        return orders
+
+    def _extract_order_from_row(self, row):
+        try:
+            date = datetime.datetime.strptime(row[0], "%m/%d/%y").date()
+            txn_type = row[1]
+            ticker = row[3]
+            num_shares = int(row[4])
+            share_price = float(row[6])
+        except Exception:
+            raise RowParserException()
+
+        if ticker in blacklisted_tickers or txn_type != 'Bought':
+            return None
+
+        return Order(ticker, date, num_shares, share_price)
+
+
+if __name__ == "__main__":
+    EtradeIngestor().run()
