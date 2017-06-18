2bed97fbc891880171dac4d0c9497dd9c98e6abf
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2017-06-18 12:17:52 -0700

    Back testing some logic in the etrade ingestor

--- tests/batch/etrade_csv_ingestor_test.py
+++ tests/batch/etrade_csv_ingestor_test.py
@@ -1,11 +1,12 @@
 import argparse
 import datetime
 import mock
+import pytest
 
 from stock_analysis.batch.etrade_csv_ingestor import EtradeIngestor
-from stock_analysis.logic import order
-
-USER_ID = 1
+from stock_analysis.batch.etrade_csv_ingestor import RowParserException
+from stock_analysis.logic import order_history
+from stock_analysis import constants
 
 
 class TestEtradeCsvIngestor(object):
@@ -14,7 +15,7 @@ class TestEtradeCsvIngestor(object):
         batch = EtradeIngestor()
         args = batch.arg_parser.parse_args(['--csv-path', './path/to/data.csv'])
         assert args == argparse.Namespace(csv_path='./path/to/data.csv')
-        assert type(batch.order_logic) == order.OrderHistoryLogic
+        assert type(batch.order_logic) == order_history.OrderHistoryLogic
 
     def test_run(self):
         batch = EtradeIngestor()
@@ -34,7 +35,25 @@ class TestEtradeCsvIngestor(object):
             batch.run()
             assert patch_parse_args.called
             assert patch_parse_orders.call_args_list == [mock.call(csv_path)]
-            assert patch_add_orders.call_args_list == [mock.call(USER_ID, mock_parsed_orders)]
+            assert patch_add_orders.call_args_list == [mock.call(constants.USER_ID, mock_parsed_orders)]
+
+    def test_parse_orders_from_csv(self):
+        csv_path = '/path/to/csv'
+        mock_reader = mock.Mock()
+        mock_orders = mock.Mock()
+        batch = EtradeIngestor()
+        with \
+                mock.patch('builtins.open') as patch_open,\
+                mock.patch('csv.reader', return_value=mock_reader) as patch_reader,\
+                mock.patch.object(
+                    batch, 'parse_orders_from_csv_reader', return_value=mock_orders
+                ) as patch_parse_orders:
+
+            orders = batch.parse_orders_from_csv(csv_path)
+            assert patch_open.call_args_list == [mock.call(csv_path)]
+            assert patch_reader.called
+            assert patch_parse_orders.call_args_list == [mock.call(mock_reader)]
+            assert orders == mock_orders
 
     def test_parse_orders_from_csv_reader(self):
         batch = EtradeIngestor()
@@ -46,7 +65,35 @@ class TestEtradeCsvIngestor(object):
         ]
         orders = batch.parse_orders_from_csv_reader(reader)
         assert set(orders) == set([
-            order.Order('NFLX', datetime.datetime(2017, 6, 12).date(), 19, 153.6799),
-            order.Order('NFLX', datetime.datetime(2017, 6, 8).date(), 39, 151.9),
+            order_history.Order('NFLX', datetime.datetime(2017, 6, 12).date(), 19, 153.6799),
+            order_history.Order('NFLX', datetime.datetime(2017, 6, 8).date(), 39, 151.9),
         ])
-        print(orders)
+
+    def test_extract_order_from_row_skips_malformed_row(self):
+        batch = EtradeIngestor()
+        row = 'MALFORMED_DATE,Bought,EQ,NFLX,39,-2924.46,151.9,4.95,NETFLIX COM INC'.split(',')
+
+        with pytest.raises(RowParserException):
+            batch.extract_order_from_row(row)
+
+    def test_extract_order_from_row_ignores_non_bought_txn_type(self):
+        batch = EtradeIngestor()
+        row = '06/12/17,UKNOWN_TXN_TYPE,EQ,NFLX,39,-2924.46,151.9,4.95,NETFLIX COM INC'.split(',')
+
+        order = batch.extract_order_from_row(row)
+        assert order is None
+
+    def test_parse_orders_from_csv_reader_skips_malformed_lines(self):
+        batch = EtradeIngestor()
+
+        # the reader is reader in a loop (i.e., for row in reader:)
+        reader = [
+            '06/12/17,Bought,EQ,NFLX,19,-2924.87,153.6799,4.95,NETFLIX COM INC'.split(','),
+            '06/08/17,Bought,EQ,NFLX,39,-2924.46,151.9,4.95,NETFLIX COM INC'.split(','),
+            'MALFORMED_DATE,Bought,EQ,NFLX,39,-2924.46,151.9,4.95,NETFLIX COM INC'.split(','),
+        ]
+        orders = batch.parse_orders_from_csv_reader(reader)
+        assert set(orders) == set([
+            order_history.Order('NFLX', datetime.datetime(2017, 6, 12).date(), 19, 153.6799),
+            order_history.Order('NFLX', datetime.datetime(2017, 6, 8).date(), 39, 151.9),
+        ])
