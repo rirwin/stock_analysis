b6b05e892fd13a136db0ac0ea2eb43788dc590c5
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2017-06-19 21:24:40 -0700

    Adding stock value logic

--- stock_analysis/logic/gain_history.py
+++ stock_analysis/logic/gain_history.py
@@ -28,3 +28,20 @@ class GainHistoryLogic(object):
         final_price = result[0]
         final_value = final_price * num_shares
         return 100 * (final_value - initial_value) / initial_value
+
+    def get_stock_value(self, user_id, ticker):
+        session = Session()
+        result = session.query(OrderHistory.price, OrderHistory.num_shares)\
+            .filter_by(ticker=ticker)\
+            .filter_by(user_id=user_id)\
+            .all()
+        initial_value = sum(x.price * x.num_shares for x in result)
+        num_shares = sum(x.num_shares for x in result)
+
+        max_date = price_logic.get_max_date_history_for_ticker(ticker)
+        result = session.query(PriceHistory.price)\
+            .filter_by(ticker=ticker)\
+            .filter_by(date=max_date.isoformat())\
+            .first()
+        final_price = result[0]
+        return final_price * num_shares
--- tests/logic/gain_logic_test.py
+++ tests/logic/gain_logic_test.py
@@ -78,3 +78,38 @@ class TestGainHistoryLogic(object):
         final_value = (order1.num_shares + order2.num_shares) * prices[-1].price
         assert self.gain_logic.get_percent_gain(self.user_id, order1.ticker) == \
             100 * (final_value - initial_value) / initial_value
+
+    @db.in_sandbox
+    def test_get_stock_value(self):
+        order1 = Order(
+            date=datetime.date(2017, 6, 12),
+            ticker='AAPL',
+            num_shares=2,
+            price=150.0,
+        )
+        order2 = Order(
+            date=datetime.date(2017, 6, 19),
+            ticker='AAPL',
+            num_shares=3,
+            price=170.0,
+        )
+        prices = [
+            TickerDatePrice(
+                ticker='AAPL',
+                date=order1.date + datetime.timedelta(days=x),
+                price=order1.price + x
+            )
+            for x in range(5)
+        ] + [                # Needed so there are no prices on weekends
+            TickerDatePrice(
+                ticker='AAPL',
+                date=order2.date + datetime.timedelta(days=x),
+                price=order2.price + x
+            )
+            for x in range(5)
+        ]
+        self.order_logic.add_orders(self.user_id, [order1, order2])
+        self.price_logic.add_prices(prices)
+
+        final_value = (order1.num_shares + order2.num_shares) * prices[-1].price
+        assert self.gain_logic.get_stock_value(self.user_id, order1.ticker) == final_value
