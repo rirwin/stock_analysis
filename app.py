d8a841671a70fcb9ffe5ce30cd586237315feb13
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2017-07-04 12:26:16 -0700

    working MVP with decent ui, refactors needed

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 portfolio_commands = portfolio.PortfolioCommands()
 
 
-# use decorators to link the function to a url
 @app.route('/')
 def home():
     return "Hello, World!"  # return a string
 
+
 @app.route('/table_sorted')
 def table_sorted():
     return render_template('semantic.html')
+
 
 @app.route('/welcome')
 def welcome():
@@ -26,6 +27,49 @@ def welcome():
     )
 
 
-# start the server with the 'run()' method
+@app.route('/hello')
+def hello():
+    details = portfolio_commands.get_portfolio_details(1)
+    table_lines = _make_table_from_details(details)
+    return render_template(
+        'hello.html',
+        table_lines=table_lines,
+    )
+
+
+def _make_table_from_details(details):
+    fields = details[0]._fields
+    table_lines = ['<table class="ui sortable celled table table-hover">']
+    table_lines.append('<thead>')
+    table_lines.append('<th data-sort="string">{}</th>'.format(fields[0]))
+    for field in fields[1:]:
+        table_lines.append('<th data-sort="customfloat">{}</th>'.format(field))
+    table_lines.append('</thead>')
+    table_lines.append('<tbody>')
+    for detail in details:
+        table_lines.append('<tr>')
+        table_lines.append('<td>{}</td>'.format(detail[0]))
+
+        for field in fields[1:]:
+            td_data = detail.__getattribute__(field)
+            is_percent = field.endswith('p')
+            is_highlighted = '1d' in field
+
+            if is_highlighted:
+                tag_coloring = 'style="color:green;text-align:right;"' if td_data >= 0 else 'style="color:red;text-align:right;"'
+                td_tag = '<td {}>'.format(tag_coloring)
+            else:
+                td_tag = '<td style="text-align:right;">'
+
+            td_template = '{0:.2f}%' if is_percent else '${0:.2f}'
+            td_data_formatted = td_template.format(td_data)
+
+            table_lines.append('{0}{1}</td>'.format(td_tag, td_data_formatted))
+        table_lines.append('</tr>')
+    table_lines.append('</tbody>')
+    table_lines.append('</table>')
+    return table_lines
+
+
 if __name__ == '__main__':
     app.run(debug=True)
