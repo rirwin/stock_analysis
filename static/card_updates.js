3ddcce7f83ace1a8a19ddc3c3b406d481a3b42c0
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2017-07-19 12:34:41 -0400

    renames of functions in test/static template

--- static/card_updates.js
+++ static/card_updates.js
@@ -1,32 +1,31 @@
-function myReset() {
+function reset_rows() {
     var table_div = document.getElementById("main_table_div");
     var rows = table_div.getElementsByTagName("tr");
     var row_i;
     for (row_i = 0; row_i < rows.length; row_i++)
         rows[row_i].classList.remove('active');
-    recalculate();
+    calculate_cards();
 }
-function myInvert() {
+function invert_selection() {
     var table_div = document.getElementById("main_table_div");
     var rows = table_div.getElementsByTagName("tr");
     var row_i;
     for (row_i = 1; row_i < rows.length; row_i++)
         rows[row_i].classList.toggle('active')
-
-    recalculate()
+    calculate_cards();
 }
-function myActivate(obj) {
+function highlight_row(obj) {
     obj.classList.toggle('active')
-    recalculate()
+    calculate_cards()
 }
-function recalculate() {
+function calculate_cards() {
     var table_div = document.getElementById("main_table_div");
     var rows = table_div.getElementsByTagName("tr");
     var row_i;
     var active_sum = 0;
     var inactive_sum = 0;
     for (row_i = 1; row_i < rows.length; row_i++) {
-        var x = rows[row_i].getElementsByClassName("my_value");
+        var x = rows[row_i].getElementsByClassName("num_shares");
         var count_row = parseFloat(x[0].innerHTML, 10);
         if ( rows[row_i].classList.contains('active') )
             active_sum = active_sum + count_row;
@@ -34,8 +33,7 @@ function recalculate() {
             inactive_sum = inactive_sum + count_row;
     }
     if ( active_sum > 0.1 )
-        document.getElementById("market_value").innerHTML = active_sum
+        document.getElementById("value_card").innerHTML = active_sum
     else
-        document.getElementById("market_value").innerHTML = inactive_sum
+        document.getElementById("value_card").innerHTML = inactive_sum
 }
-
